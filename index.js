7b7e1c2410e959240e5706b6dd636613fce6ee43
BangCD <harmanhssingh2001@gmail.com>
2022-09-03 05:54:23 +0400

    revert

--- index.js
+++ index.js
@@ -1,15 +1,3 @@
-const http = require('http');
- 
-const server = http.createServer((req, res) => {
-// Set the response HTTP header with HTTP status and Content type
-res.writeHead(200, {'Content-Type': 'text/plain'});
-// Send the response body "Hello World"
-res.end('Just for testing purposes\n');
-});
- 
-server.listen(port, () => {
-console.log('Hello world listening on port', port);
-});
 const Discord=require("discord.js");
 const { MessageEmbed } = require('discord.js');
 const { Client, Intents } = require('discord.js');
